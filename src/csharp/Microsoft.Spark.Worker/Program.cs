abd0494f1b8a1fadb862648482d2a29e6460d224
karlgodtliebsen <karl@godtliebsen.dk>
2023-09-26 21:50:59 +0200

    Environment variable verification in Microsoft.Spark.Worker project

--- src/csharp/Microsoft.Spark.Worker/Program.cs
+++ src/csharp/Microsoft.Spark.Worker/Program.cs
@@ -5,41 +5,36 @@
 using System;
 using System.Diagnostics;
 
-namespace Microsoft.Spark.Worker
+using Microsoft.Spark.Worker;
+
+
+var sparkVersionEnvironmentVariable = Environment.GetEnvironmentVariable("DOTNET_WORKER_SPARK_VERSION");
+ArgumentNullException.ThrowIfNull(sparkVersionEnvironmentVariable);
+var sparkVersion = new Version(sparkVersionEnvironmentVariable);
+
+// Note that for the daemon server, standard output is used to communicate
+// port number. Thus, use error output until the communication is complete.
+Console.Error.WriteLine(
+    $"DotnetWorker PID:[{Process.GetCurrentProcess().Id}] " +
+    $"Args:[{string.Join(" ", args)}] " +
+    $"SparkVersion:[{sparkVersion}]");
+
+if (args.Length != 2)
 {
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            var sparkVersion = new Version(
-                Environment.GetEnvironmentVariable("DOTNET_WORKER_SPARK_VERSION"));
+    Console.Error.WriteLine($"Invalid number of args: {args.Length}");
+    Environment.Exit(-1);
+}
 
-            // Note that for the daemon server, standard output is used to communicate
-            // port number. Thus, use error output until the communication is complete.
-            Console.Error.WriteLine(
-                $"DotnetWorker PID:[{Process.GetCurrentProcess().Id}] " +
-                $"Args:[{string.Join(" ", args)}] " +
-                $"SparkVersion:[{sparkVersion}]");
-
-            if (args.Length != 2)
-            {
-                Console.Error.WriteLine($"Invalid number of args: {args.Length}");
-                Environment.Exit(-1);
-            }
-
-            if ((args[0] == "-m") && (args[1] == "pyspark.worker"))
-            {
-                new SimpleWorker(sparkVersion).Run();
-            }
-            else if ((args[0] == "-m") && args[1] == ("pyspark.daemon"))
-            {
-                new DaemonWorker(sparkVersion).Run();
-            }
-            else
-            {
-                Console.Error.WriteLine("Unknown options received.");
-                Environment.Exit(-1);
-            }
-        }
-    }
+if ((args[0] == "-m") && (args[1] == "pyspark.worker"))
+{
+    new SimpleWorker(sparkVersion).Run();
+}
+else if ((args[0] == "-m") && args[1] == ("pyspark.daemon"))
+{
+    new DaemonWorker(sparkVersion).Run();
+}
+else
+{
+    Console.Error.WriteLine("Unknown options received.");
+    Environment.Exit(-1);
 }
